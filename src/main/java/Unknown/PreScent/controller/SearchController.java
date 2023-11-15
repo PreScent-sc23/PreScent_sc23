aba3a6023369ee1e74f176a9ee622ccda2ef6506
sion5031 <swseo97@ajou.ac.kr>
2023-11-15 16:17:28 +0900

    Fix: SearchController Get fix

--- src/main/java/Unknown/PreScent/controller/SearchController.java
+++ src/main/java/Unknown/PreScent/controller/SearchController.java
@@ -11,7 +11,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +24,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,9 +40,35 @@ public class SearchController {
     @Autowired
     private SearchService searchService;
 
+//    @GetMapping("/search")
+//    public ResponseEntity<Map<List<FinishedProductEntity>, Object>> searchTag(@RequestParam String query,
+//                                                                HttpServletRequest request, HttpServletResponse response){
+//        System.out.println("Qurey: " + query + "----------------\n");
+//
+//        if(query.startsWith("#")){
+//            String[] queryResult = query.split("#");
+//
+//            System.out.println("query split: " + queryResult[0] + "----------------\n");
+//
+//            Map<List<FinishedProductEntity>,Object> result = new HashMap<>();
+//            Optional<List<FinishedProductEntity>> searchResult = searchService.searchByTagDefault(queryResult[0]);
+////            Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+//            result.put(searchResult.get(), request.getAttribute(ERROR_EXCEPTION));
+////            result.put("Message", ex.getMessage());
+//
+//            Integer statusCode = (Integer) request.getAttribute(ERROR_EXCEPTION);
+//
+//            return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+//        }
+//
+//        return ResponseEntity.noContent().build();
+//
+//    }
+
+
     @GetMapping("/search")
-    public ResponseEntity<Map<List<FinishedProductEntity>, Object>> searchTag(@RequestParam String query,
-                                                                HttpServletRequest request, HttpServletResponse response){
+    public ResponseEntity<List<FinishedProductEntity>> searchTag(@RequestParam String query,
+                                                                              HttpServletRequest request, HttpServletResponse response){
         System.out.println("Qurey: " + query + "----------------\n");
 
         if(query.startsWith("#")){
@@ -47,19 +76,20 @@ public class SearchController {
 
             System.out.println("query split: " + queryResult[0] + "----------------\n");
 
-            Map<List<FinishedProductEntity>,Object> result = new HashMap<>();
             Optional<List<FinishedProductEntity>> searchResult = searchService.searchByTagDefault(queryResult[0]);
-//            Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
-            result.put(searchResult.get(), request.getAttribute(ERROR_EXCEPTION));
-//            result.put("Message", ex.getMessage());
+            List<FinishedProductEntity> result = searchResult.get();
 
             Integer statusCode = (Integer) request.getAttribute(ERROR_EXCEPTION);
 
-            return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));//????
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+            return new ResponseEntity<>(result, headers, HttpStatus.OK);
         }
 
         return ResponseEntity.noContent().build();
 
     }
+
 
 }
