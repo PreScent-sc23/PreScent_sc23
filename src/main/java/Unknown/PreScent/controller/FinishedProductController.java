b6653c6fbbfa527dc345d2e905c5415a8a7259c7
sion5031 <swseo97@ajou.ac.kr>
2023-11-22 22:51:18 +0900

    Feat: PreScentApplication log added (9)

--- src/main/java/Unknown/PreScent/controller/FinishedProductController.java
+++ src/main/java/Unknown/PreScent/controller/FinishedProductController.java
@@ -49,13 +49,13 @@ public class FinishedProductController {
 //    } //old version
 
     @PostMapping("/add")
-    public ResponseEntity<?> addFinishedProduct(@RequestPart Integer shopKey,
-                                                @RequestPart(name = "fpImage") MultipartFile fpImage,
-                                                @RequestPart("fpName") String fpName,
-                                                @RequestPart("fpTag") String fpTag,
-                                                @RequestPart("fpPrice") Integer fpPrice,
-                                                @RequestPart("fpDetail") String fpDetail,
-                                                @RequestPart("fpFlowerList") String fpFlowerList)
+    public ResponseEntity<?> addFinishedProduct(@RequestParam Integer shopKey,
+                                                @RequestParam(name = "fpImage", required = false) MultipartFile fpImage,
+                                                @RequestParam("fpName") String fpName,
+                                                @RequestParam("fpTag") String fpTag,
+                                                @RequestParam("fpPrice") Integer fpPrice,
+                                                @RequestParam("fpDetail") String fpDetail,
+                                                @RequestParam("fpFlowerList") String fpFlowerList)
     {
         System.out.println("shopKey 값 : "+shopKey+"--------------------------------------------");
         System.out.println("fpName 값 : "+fpName+"--------------------------------------------");
