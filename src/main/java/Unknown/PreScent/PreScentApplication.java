4d4b7f7c8e3ccb989696863a734769505e0eeadf
sip-do <rlaehdbs20@gmail.com>
2023-11-25 21:16:15 +0900

    Reverting to the abd3d75

---
+++ src/main/java/Unknown/PreScent/PreScentApplication.java
@@ -0,0 +1,13 @@
+package Unknown.PreScent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreScentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreScentApplication.class, args);
+	}
+	int aaa;
+}
