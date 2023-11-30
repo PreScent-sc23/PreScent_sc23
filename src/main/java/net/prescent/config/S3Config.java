b95bd6d5dedc9e606b64dcec2fa6c9eb0b5b8c72
sip-do <rlaehdbs20@gmail.com>
2023-11-30 18:39:58 +0900

    upload for server

--- src/main/java/net/prescent/config/S3Config.java
+++ src/main/java/net/prescent/config/S3Config.java
@@ -12,26 +12,26 @@ import com.amazonaws.services.s3.AmazonS3;
 
 @Configuration
 public class S3Config {
-//    @Value("${cloud.aws.credentials.access-key}")
-//    private String accessKey;
-//    @Value("${cloud.aws.credentials.secret-key}")
-//    private String secretKey;
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Bean
-    public AmazonS3 amazonS3() {
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .build();
-    }
 //    @Bean
-//    public AmazonS3Client amazonS3Client() {
-//        BasicAWSCredentials awsCredentials= new BasicAWSCredentials(accessKey, secretKey);
-//        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+//    public AmazonS3 amazonS3() {
+//        return AmazonS3ClientBuilder.standard()
 //                .withRegion(region)
-//                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
 //                .build();
 //    }
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials awsCredentials= new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
 
 }
