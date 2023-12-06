86dd80944c4ef3f354572644e7dccf5ae9663c66
susooo <92291198+susooo@users.noreply.github.com>
2023-12-07 04:34:42 +0900

    Feat: connect test with front

--- src/main/java/net/prescent/config/S3Config.java
+++ src/main/java/net/prescent/config/S3Config.java
@@ -19,12 +19,13 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    //    @Bean
-//    public AmazonS3 amazonS3() {
-//        return AmazonS3ClientBuilder.standard()
-//                .withRegion(region)
-//                .build();
-//    }
+    @Bean
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+        .withRegion(region)
+        .build();
+    }
+
     @Bean
     public AmazonS3Client amazonS3Client() {
         BasicAWSCredentials awsCredentials= new BasicAWSCredentials(accessKey, secretKey);
