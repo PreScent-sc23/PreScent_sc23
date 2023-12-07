279f226407f54ea5298019b3478bdc5c5ca28e01
sip-do <rlaehdbs20@gmail.com>
2023-12-07 12:10:46 +0900

    main merge with sooh

--- src/test/java/net/prescent/service/CartServiceTest.java
+++ src/test/java/net/prescent/service/CartServiceTest.java
@@ -19,7 +19,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@Transactional
+//@Transactional
 @SpringBootTest
 @Slf4j
 public class CartServiceTest {
@@ -37,8 +37,8 @@ public class CartServiceTest {
     CustomerRepository customerRepo;
     @Autowired
     PasswordEncoder passwordEncoder;
-    private Integer tempNumForTest = 1;
-    private String  tempStringForTest = "0";
+    private Integer tempNumForTest = 0;
+    private String  tempStringForTest = "";
     //    @BeforeEach
 //    public void setup()
 //    {
@@ -80,14 +80,14 @@ public class CartServiceTest {
         finishedProductDto.setGetFpFlowerList(arr);
         return finishedProductDto;
     }
-    public FinishedProductDto createFinishedProductDto(String fpDetail, String fpImage, String[] fpFlowerList, String fpName, String fpTag, Integer fpPrice) {
+    public FinishedProductDto createFinishedProductDto(String fpDetail, String fpImage, String fpFlowerList, String fpName, String fpTag, Integer fpPrice) {
         FinishedProductDto finishedProductDto = new FinishedProductDto();
         finishedProductDto.setFpDetail(fpDetail);
         finishedProductDto.setFpName(fpName);
         finishedProductDto.setFpTag(fpTag);
         finishedProductDto.setFpImage(fpImage);
         finishedProductDto.setFpPrice(fpPrice);
-        finishedProductDto.setFpFlowerList(fpFlowerList);
+        finishedProductDto.setGetFpFlowerList(fpFlowerList);
         return finishedProductDto;
     }
     private CustomerDto createTestCustomerDto() {
@@ -101,60 +101,9 @@ public class CartServiceTest {
         return customerDto;
     }
     //테스트 목록. 주문서 작성.
-    @Test
-    @DisplayName("카트 추가 테스트")
-    public void addInCartTest() {
-        CustomerDto customerDto = createTestCustomerDto();
-        Integer customerKey = userService.signupCustomer(customerDto);
-        assertNotNull(customerKey);
-
-        SellerDto sellerDto = createSellerDto();
-        Integer sellerKey = userService.signupSeller(sellerDto);
-
-        FlowerShopDto flowerShopDto = createFlowerShopDto();
-        FlowerShopEntity addedShop = flowerShopService.addFlowerShop(flowerShopDto);
-
-        FinishedProductDto finishedProductDto = createFinishedProductDto();
-        finishedProductDto.setShopKey(addedShop.getShopKey());
-        assertNotNull(addedShop.getShopKey());
-        FinishedProductEntity addedFinishedProductEntity = finishedProductService.addFinishedProduct(finishedProductDto);
-
-        CartItemAddRequestDto cartItemAddRequestDto = new CartItemAddRequestDto(customerKey, addedFinishedProductEntity.getFpKey(), 1, "20231204", "18:00");
-        cartService.addToCart(cartItemAddRequestDto);
-        assertNotNull(customerRepo.findByUserKey(customerKey).get().getCartEntity().getCartItemEntityList());
-    }
-
-    @Test
-    @DisplayName("카트정보 조회 테스트")
-    public void viewInCartTest() {
-        CustomerDto customerDto = createTestCustomerDto();
-        Integer customerKey = userService.signupCustomer(customerDto);
-        assertNotNull(customerKey);
-
-        SellerDto sellerDto = createSellerDto();
-        Integer sellerKey = userService.signupSeller(sellerDto);
-
-        FlowerShopDto flowerShopDto = createFlowerShopDto();
-        FlowerShopEntity addedShop = flowerShopService.addFlowerShop(flowerShopDto);
-
-        FinishedProductDto finishedProductDto = createFinishedProductDto();
-        finishedProductDto.setShopKey(addedShop.getShopKey());
-        assertNotNull(addedShop.getShopKey());
-        FinishedProductEntity addedFinishedProductEntity = finishedProductService.addFinishedProduct(finishedProductDto);
-
-        CartItemAddRequestDto cartItemAddRequestDto = new CartItemAddRequestDto(customerKey, addedFinishedProductEntity.getFpKey(), 1, "20231204", "18:00");
-
-        cartService.addToCart(cartItemAddRequestDto);
-        assertNotNull(customerRepo.findByUserKey(customerKey).get().getCartEntity().getCartItemEntityList());
-
-        List<CartItemResponseDto> cartItemResponseDtoList = cartService.viewInCart(customerKey);
-        assertEquals(cartItemResponseDtoList.get(0).getFlowerShopName(),flowerShopDto.getShopName());
-        log.info(cartItemResponseDtoList.toString());
-    }
-
 //    @Test
-//    @DisplayName("db추가")
-//    public void addToDB(){
+//    @DisplayName("카트 추가 테스트")
+//    public void addInCartTest() {
 //        CustomerDto customerDto = createTestCustomerDto();
 //        Integer customerKey = userService.signupCustomer(customerDto);
 //        assertNotNull(customerKey);
@@ -165,46 +114,122 @@ public class CartServiceTest {
 //        FlowerShopDto flowerShopDto = createFlowerShopDto();
 //        FlowerShopEntity addedShop = flowerShopService.addFlowerShop(flowerShopDto);
 //
-//        FinishedProductDto finishedProductDto0 = createFinishedProductDto("메리골드가 돋보이는 화이트데이 꽃다발입니다","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/orange.jpg","메리골드", "라넌큘려스", "사탕수수"},"메리골드 다발","#화이트데이", 63000);
-//        finishedProductDto0.setShopKey(addedShop.getShopKey());
+//        FinishedProductDto finishedProductDto = createFinishedProductDto();
+//        finishedProductDto.setShopKey(addedShop.getShopKey());
 //        assertNotNull(addedShop.getShopKey());
-//        FinishedProductEntity addedFinishedProductEntity0 = finishedProductService.addFinishedProduct(finishedProductDto0);
+//        FinishedProductEntity addedFinishedProductEntity = finishedProductService.addFinishedProduct(finishedProductDto);
 //
-//        FinishedProductDto finishedProductDto1 = createFinishedProductDto("색의 조화가 매력적인 꽃다발입니다","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/fall.jpg",new String[]{"장미", "안개"},"라넌큘러스 다발","#화이트데이",85000);
-//        finishedProductDto1.setShopKey(addedShop.getShopKey());
-//        assertNotNull(addedShop.getShopKey());
-//        FinishedProductEntity addedFinishedProductEntity1 = finishedProductService.addFinishedProduct(finishedProductDto1);
-//
-//        FinishedProductDto finishedProductDto2 = createFinishedProductDto("핑크라넌과 코스모스를 조합한 꽃다발 입니다","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/pink.jpg",new String[]{"라넌", "코스모스"},"핑크라넌","#화이트데이",73000);
-//        finishedProductDto2.setShopKey(addedShop.getShopKey());
-//        assertNotNull(addedShop.getShopKey());
-//        FinishedProductEntity addedFinishedProductEntity2 = finishedProductService.addFinishedProduct(finishedProductDto2);
-//
-//        FinishedProductDto finishedProductDto3 = createFinishedProductDto("봄의 화사함을 담은 화이트데이 꽃다발입니다","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/spring.jpg", new String[]{"개나리", "진달래"},"스프링 델피늄","#화이트데이",55000);
-//        finishedProductDto3.setShopKey(addedShop.getShopKey());
-//        assertNotNull(addedShop.getShopKey());
-//        FinishedProductEntity addedFinishedProductEntity3 = finishedProductService.addFinishedProduct(finishedProductDto3);
-//
-//        FinishedProductDto finishedProductDto4 = createFinishedProductDto("미니 델피늄이 한가득 담긴 꽃다발입니다","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/winter.jpg", new String[]{"델피늄", "한가득"},"델피늄 한가득","#화이트데이",55000);
-//        finishedProductDto4.setShopKey(addedShop.getShopKey());
-//        assertNotNull(addedShop.getShopKey());
-//        FinishedProductEntity addedFinishedProductEntity4 = finishedProductService.addFinishedProduct(finishedProductDto4);
-//
-//        FinishedProductDto finishedProductDto5 = createFinishedProductDto("곰인형과 화이트로즈로 구성된 화이트데이 꽃다발","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/lovely.png",new String[]{"장미", "라넌큘러스"},"러블리 꽃다발","#화이트데이",49000);
-//        finishedProductDto5.setShopKey(addedShop.getShopKey());
-//        assertNotNull(addedShop.getShopKey());
-//        FinishedProductEntity addedFinishedProductEntity5 = finishedProductService.addFinishedProduct(finishedProductDto5);
-//
-//        CartItemAddRequestDto cartItemAddRequestDto = new CartItemAddRequestDto(customerKey, addedFinishedProductEntity0.getFpKey(), 1, "20231204", "18:00");
+//        CartItemAddRequestDto cartItemAddRequestDto = new CartItemAddRequestDto(customerKey, addedFinishedProductEntity.getFpKey(), 1, "20231204", "18:00");
 //        cartService.addToCart(cartItemAddRequestDto);
-//        CartItemAddRequestDto cartItemAddRequestDto1 = new CartItemAddRequestDto(customerKey, addedFinishedProductEntity1.getFpKey(), 1, "20231204", "18:00");
-//        cartService.addToCart(cartItemAddRequestDto1);
-//        CartItemAddRequestDto cartItemAddRequestDto2 = new CartItemAddRequestDto(customerKey, addedFinishedProductEntity2.getFpKey(), 1, "20231204", "18:00");
-//        cartService.addToCart(cartItemAddRequestDto2);
+//        assertNotNull(customerRepo.findByUserKey(customerKey).get().getCartEntity().getCartItemEntityList());
+//    }
+//
+//    @Test
+//    @DisplayName("카트정보 조회 테스트")
+//    public void viewInCartTest() {
+//        CustomerDto customerDto = createTestCustomerDto();
+//        Integer customerKey = userService.signupCustomer(customerDto);
+//        assertNotNull(customerKey);
+//
+//        SellerDto sellerDto = createSellerDto();
+//        Integer sellerKey = userService.signupSeller(sellerDto);
+//
+//        FlowerShopDto flowerShopDto = createFlowerShopDto();
+//        FlowerShopEntity addedShop = flowerShopService.addFlowerShop(flowerShopDto);
+//
+//        FinishedProductDto finishedProductDto = createFinishedProductDto();
+//        finishedProductDto.setShopKey(addedShop.getShopKey());
+//        assertNotNull(addedShop.getShopKey());
+//        FinishedProductEntity addedFinishedProductEntity = finishedProductService.addFinishedProduct(finishedProductDto);
+//
+//        CartItemAddRequestDto cartItemAddRequestDto = new CartItemAddRequestDto(customerKey, addedFinishedProductEntity.getFpKey(), 1, "20231204", "18:00");
+//
+//        cartService.addToCart(cartItemAddRequestDto);
 //        assertNotNull(customerRepo.findByUserKey(customerKey).get().getCartEntity().getCartItemEntityList());
 //
 //        List<CartItemResponseDto> cartItemResponseDtoList = cartService.viewInCart(customerKey);
 //        assertEquals(cartItemResponseDtoList.get(0).getFlowerShopName(),flowerShopDto.getShopName());
 //        log.info(cartItemResponseDtoList.toString());
 //    }
+
+    @Test
+    @DisplayName("db추가")
+    public void addToDB(){
+        CustomerDto customerDto = createTestCustomerDto();
+        Integer customerKey = userService.signupCustomer(customerDto);
+        assertNotNull(customerKey);
+
+        SellerDto sellerDto = createSellerDto();
+        Integer sellerKey = userService.signupSeller(sellerDto);
+
+        FlowerShopDto flowerShopDto = createFlowerShopDto();
+        FlowerShopEntity addedShop = flowerShopService.addFlowerShop(flowerShopDto);
+
+        FinishedProductDto finishedProductDto0 = createFinishedProductDto("메리골드가 돋보이는 화이트데이 꽃다발입니다","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/orange.jpg","메리골드 라넌큘려스 사탕수수","메리골드 다발","#화이트데이", 100);
+        finishedProductDto0.setShopKey(addedShop.getShopKey());
+        assertNotNull(addedShop.getShopKey());
+        FinishedProductEntity addedFinishedProductEntity0 = finishedProductService.addFinishedProduct(finishedProductDto0);
+
+        FinishedProductDto finishedProductDto1 = createFinishedProductDto("색의 조화가 매력적인 꽃다발입니다","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/fall.jpg","장미 안개","라넌큘러스 다발","#화이트데이",100);
+        finishedProductDto1.setShopKey(addedShop.getShopKey());
+        assertNotNull(addedShop.getShopKey());
+        FinishedProductEntity addedFinishedProductEntity1 = finishedProductService.addFinishedProduct(finishedProductDto1);
+
+        FinishedProductDto finishedProductDto2 = createFinishedProductDto("핑크라넌과 코스모스를 조합한 꽃다발 입니다","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/pink.jpg","라넌 코스모스","핑크라넌","#화이트데이",100);
+        finishedProductDto2.setShopKey(addedShop.getShopKey());
+        assertNotNull(addedShop.getShopKey());
+        FinishedProductEntity addedFinishedProductEntity2 = finishedProductService.addFinishedProduct(finishedProductDto2);
+
+        FinishedProductDto finishedProductDto3 = createFinishedProductDto("봄의 화사함을 담은 화이트데이 꽃다발입니다","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/spring.jpg", "개나리 진달래","스프링 델피늄","#화이트데이",100);
+        finishedProductDto3.setShopKey(addedShop.getShopKey());
+        assertNotNull(addedShop.getShopKey());
+        FinishedProductEntity addedFinishedProductEntity3 = finishedProductService.addFinishedProduct(finishedProductDto3);
+
+        FinishedProductDto finishedProductDto4 = createFinishedProductDto("미니 델피늄이 한가득 담긴 꽃다발입니다","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/winter.jpg", "델피늄 한가득","델피늄 한가득","#화이트데이",100);
+        finishedProductDto4.setShopKey(addedShop.getShopKey());
+        assertNotNull(addedShop.getShopKey());
+        FinishedProductEntity addedFinishedProductEntity4 = finishedProductService.addFinishedProduct(finishedProductDto4);
+
+        FinishedProductDto finishedProductDto5 = createFinishedProductDto("곰인형과 화이트로즈로 구성된 화이트데이 꽃다발","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/lovely.png","장미 라넌큘러스","러블리 꽃다발","#화이트데이",49000);
+        finishedProductDto5.setShopKey(addedShop.getShopKey());
+        assertNotNull(addedShop.getShopKey());
+        FinishedProductEntity addedFinishedProductEntity5 = finishedProductService.addFinishedProduct(finishedProductDto5);
+
+//        FinishedProductDto finishedProductDto5 = createFinishedProductDto("곰","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/lovely.png","장미 라넌큘러스","러블리 꽃다발","#화이트데이",49000);
+//        finishedProductDto5.setShopKey(addedShop.getShopKey());
+//        assertNotNull(addedShop.getShopKey());
+//        FinishedProductEntity addedFinishedProductEntity5 = finishedProductService.addFinishedProduct(finishedProductDto5);
+//
+//        FinishedProductDto finishedProductDto5 = createFinishedProductDto("곰인형과 화이트로즈로 구성된 화이트데이 꽃다발","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/lovely.png","장미 라넌큘러스","러블리 꽃다발","#화이트데이",49000);
+//        finishedProductDto5.setShopKey(addedShop.getShopKey());
+//        assertNotNull(addedShop.getShopKey());
+//        FinishedProductEntity addedFinishedProductEntity5 = finishedProductService.addFinishedProduct(finishedProductDto5);
+//
+//        FinishedProductDto finishedProductDto5 = createFinishedProductDto("곰인형과 화이트로즈로 구성된 화이트데이 꽃다발","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/lovely.png","장미 라넌큘러스","러블리 꽃다발","#화이트데이",49000);
+//        finishedProductDto5.setShopKey(addedShop.getShopKey());
+//        assertNotNull(addedShop.getShopKey());
+//        FinishedProductEntity addedFinishedProductEntity5 = finishedProductService.addFinishedProduct(finishedProductDto5);
+//
+//        FinishedProductDto finishedProductDto5 = createFinishedProductDto("곰인형과 화이트로즈로 구성된 화이트데이 꽃다발","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/lovely.png","장미 라넌큘러스","러블리 꽃다발","#화이트데이",49000);
+//        finishedProductDto5.setShopKey(addedShop.getShopKey());
+//        assertNotNull(addedShop.getShopKey());
+//        FinishedProductEntity addedFinishedProductEntity5 = finishedProductService.addFinishedProduct(finishedProductDto5);
+//
+//        FinishedProductDto finishedProductDto5 = createFinishedProductDto("곰인형과 화이트로즈로 구성된 화이트데이 꽃다발","https://prescentbucket.s3.ap-northeast-2.amazonaws.com/lovely.png","장미 라넌큘러스","러블리 꽃다발","#화이트데이",49000);
+//        finishedProductDto5.setShopKey(addedShop.getShopKey());
+//        assertNotNull(addedShop.getShopKey());
+//        FinishedProductEntity addedFinishedProductEntity5 = finishedProductService.addFinishedProduct(finishedProductDto5);
+
+        CartItemAddRequestDto cartItemAddRequestDto = new CartItemAddRequestDto(customerKey, addedFinishedProductEntity0.getFpKey(), 1, "2023년 12월04일", "18시00분");
+        cartService.addToCart(cartItemAddRequestDto);
+        CartItemAddRequestDto cartItemAddRequestDto1 = new CartItemAddRequestDto(customerKey, addedFinishedProductEntity1.getFpKey(), 1, "2023년 12월04일", "18시00분");
+        cartService.addToCart(cartItemAddRequestDto1);
+        CartItemAddRequestDto cartItemAddRequestDto2 = new CartItemAddRequestDto(customerKey, addedFinishedProductEntity2.getFpKey(), 1, "2023년 12월04일", "18시00분");
+        cartService.addToCart(cartItemAddRequestDto2);
+        assertNotNull(customerRepo.findByUserKey(customerKey).get().getCartEntity().getCartItemEntityList());
+
+        List<CartItemResponseDto> cartItemResponseDtoList = cartService.viewInCart(customerKey);
+        assertEquals(cartItemResponseDtoList.get(0).getFlowerShopName(),flowerShopDto.getShopName());
+        log.info(cartItemResponseDtoList.toString());
+    }
 }
