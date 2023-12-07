a7e34fc5ffba899dcb5f04ba9c621b274bda87d3
susooo <92291198+susooo@users.noreply.github.com>
2023-12-07 09:53:59 +0900

    Feat: 오류 해결

--- src/main/java/net/prescent/controller/ai/AIanalysisController.java
+++ src/main/java/net/prescent/controller/ai/AIanalysisController.java
@@ -31,7 +31,7 @@ public class AIanalysisController {
     public ResponseEntity<?> uploadAndProcessImage(@RequestPart("file") MultipartFile file) {
         try {
             String fileKey = file.getOriginalFilename();
-            aIs3Service.uploadFileFromPath("backend/src/main/python/detect/file.getOriginalFilename()", fileKey);
+            aIs3Service.uploadFileFromPath("backend/src/main/python/detects/file.getOriginalFilename()", fileKey);
 
             String fileUrl = aIs3Service.getFileUrl(fileKey);
             List<Map<String, Object>> additionalImages = aiTestService.processAdditionalImages(fileKey);
